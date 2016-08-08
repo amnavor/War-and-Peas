3a9fb5aa69cb1690ed8d4ab49d923c17917d0285
Amna <amnavor@gmail.com>
2016-08-08 14:15:50 -0700

    Adding the Pod character

--- War and Peas/GameScene.swift
+++ War and Peas/GameScene.swift
@@ -1,45 +1,14 @@
-//
-//  GameScene.swift
-//  War and Peas
-//
-//  Created by Amna  on 08/08/2016.
-//  Copyright (c) 2016 Amnavor. All rights reserved.
-//
-
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    // player sprite
+    let player = SKSpriteNode(imageNamed: "Pod")
+    
+    override func didMoveToView(view: SKView) {
+        backgroundColor = SKColor.whiteColor()
+        //player position is horizontally centered and vertically 10%
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        addChild(player)
     }
 }
