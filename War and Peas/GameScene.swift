9cc9b3fa3b31501dc8419161c0bc10f4a173fda0
Amna <amnavor@gmail.com>
2016-08-08 14:56:28 -0700

    Add tomatoes

--- War and Peas/GameScene.swift
+++ War and Peas/GameScene.swift
@@ -10,5 +10,50 @@ class GameScene: SKScene {
         //player position is horizontally centered and vertically 10%
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         addChild(player)
+        
+        //run "addMonster" function every 1 second
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addMonster),
+                SKAction.waitForDuration(1.0)
+                ])
+            ))
+    }
+    
+    
+    
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
+        
+        // Create sprite
+        let monster = SKSpriteNode(imageNamed: "Tomato")
+        
+        
+        // Position the monster slightly off-screen along the right edge
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        addChild(monster)
+        
+        // random speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        // Create the actions: move, then remove from screen
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
     }
 }
+
