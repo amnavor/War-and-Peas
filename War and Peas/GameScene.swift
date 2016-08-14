64ea07776ccfab6fbccef7eda12e1228e320059b
Amna <amnavor@gmail.com>
2016-08-14 12:45:07 -0700

    Points counter

--- War and Peas/GameScene.swift
+++ War and Peas/GameScene.swift
@@ -57,7 +57,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // player sprite
     let player = SKSpriteNode(imageNamed: "Pod")
-    
+    //counter
+    var monstersDestroyed = 0
     
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
@@ -129,7 +130,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
         
-        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        let loseAction = SKAction.runBlock() {
+            let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+            let gameOverScene = GameOverScene(size: self.size, won: false)
+            self.view?.presentScene(gameOverScene, transition: reveal)
+        }
+        
+        monster.runAction(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
         
     }
     
@@ -181,6 +188,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             print("Hit")
             projectile.removeFromParent()
             monster.removeFromParent()
+            monstersDestroyed++
+            if (monstersDestroyed > 30) {
+                let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+                let gameOverScene = GameOverScene(size: self.size, won: true)
+                self.view?.presentScene(gameOverScene, transition: reveal)
+            }
         }
         
         
